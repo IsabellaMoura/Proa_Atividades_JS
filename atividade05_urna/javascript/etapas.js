8dd35c791f2eb4ba07ba0267e1efe811bbbd1231
IsabellaMoura <70759293+IsabellaMoura@users.noreply.github.com>
2021-11-18 06:48:54 -0300

    Add files via upload

---
+++ atividade05_urna/javascript/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '00000',
+                nome: 'Fernando',
+                partido: 'CcP',
+                fotos: [
+                    { url: '6.jpg', legenda: 'Vereador', small: false }
+                ]
+            },
+            {
+                numero: '10000',
+                nome: 'Maria',
+                partido: 'CsP',
+                fotos: [
+                    { url: '5.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '00',
+                nome: 'Júlia',
+                partido: 'CcP',
+                vice: 'Bianca',
+                fotos: [
+                    { url: '2.png', legenda: 'Prefeito' },
+                    { url: '3.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '10',
+                nome: 'Gabriel',
+                partido: 'CsP',
+                vice: 'Amanda',
+                fotos: [
+                    { url: '1.png', legenda: 'Prefeito' },
+                    { url: '4.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
